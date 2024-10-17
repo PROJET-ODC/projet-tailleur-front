9287c8c7f14ca09b8ea8f67526f823817814141d
Ramaseck1 <seckrama022@gmail.com>
2024-10-17 04:27:04 +0000

    posts add cart

--- src/components/principal/navbar/items/FaShoppingCart.jsx
+++ src/components/principal/navbar/items/FaShoppingCart.jsx
@@ -2,24 +2,21 @@ import Loader from "./itemsComponents/sousComponentitems/Loader";
 import CartHeader from "./itemsComponents/sousComponentitems/CartHeader";
 import CartList from "./itemsComponents/sousComponentitems/CartList";
 
-const FaShoppingCart = () => {
+const FaShoppingCart = ({ cart }) => {
+  const total = Array.isArray(cart) ? cart.reduce((acc, item) => acc + (parseFloat(item.price) || 0), 0) : 0; // Vérifiez si cart est un tableau
+
   return (
     <div className="navbar-item is-cart">
       <div className="cart-button">
         <i data-feather="shopping-cart"></i>
-        <div className="cart-count"></div>
+        <div className="cart-count">1</div>
       </div>
 
       <div className="shopping-cart">
         <div className="cart-inner">
           <Loader />
-
-          {/* En-tête du panier */}
-          <CartHeader total="$193.00" />
-
-          {/* Liste des articles */}
-          <CartList />
-
+          <CartHeader total={`$${total.toFixed(2)}`} />
+          <CartList items={cart} />
           <a href="#" className="button primary-button is-raised">
             Checkout
           </a>
@@ -28,5 +25,8 @@ const FaShoppingCart = () => {
     </div>
   );
 };
+
+
+
 
 export default FaShoppingCart;
